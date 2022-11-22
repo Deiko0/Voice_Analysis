eb70a3ed40816385063f3da9bd58c45eae951329
Deiko0 <118802900+Deiko0@users.noreply.github.com>
2022-11-22 18:58:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -170,7 +170,8 @@ def main():
         href = f'<a href="data:application/octet-stream;base64,{b64}" download="result.csv">download</a>'
         st.markdown(f"csvファイルでダウンロード {href}", unsafe_allow_html=True)
         st.write('基本周波数とHNRは平均で計算しています。')
-        st.write('create by Deiko')
+        
+   　st.write('create by Deiko')
 
 if __name__ == "__main__":
     _set_block_container_style()
